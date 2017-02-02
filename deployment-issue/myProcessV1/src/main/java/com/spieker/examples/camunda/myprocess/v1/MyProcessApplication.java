6eb94bfc010cd4b5ee71f32498a0c3f92d3401df
cspi <carsten.spieker@d-velop.de>
2017-02-02 11:46:46 +0100

    Added failure example

---
+++ deployment-issue/myProcessV1/src/main/java/com/spieker/examples/camunda/myprocess/v1/MyProcessApplication.java
@@ -0,0 +1,9 @@
+package com.spieker.examples.camunda.myprocess.v1;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("My Process")
+public class MyProcessApplication extends ServletProcessApplication {
+	  // empty implementation
+}
